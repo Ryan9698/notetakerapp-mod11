5a39455dc7b6f031e582d232dc3f55d4ce3fbad6
Ryan Caltabiano <rcaltabiano90@gmail.com>
2023-11-09 11:52:20 -0500

    Push for BCS

--- Develop/server.js
+++ Develop/server.js
@@ -3,14 +3,27 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 let notes = [];
 
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
-  });
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.json(notes);
+  return notes;
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+// app.get('/api/notes', (req, res) => {
+//     res.json(notes);
+//     return notes;
+//   });
+
 
 app.use(express.static('public'));
 
